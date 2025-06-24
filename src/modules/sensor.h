ed6c17630199e0c5a663ca514718673a4cf4257f
purin <puriso@users.noreply.github.com>
2025-06-24 23:15:27 +0900

    Fix workflow and header

--- src/modules/sensor.h
+++ src/modules/sensor.h
@@ -12,22 +12,8 @@ extern float oilPressureSamples[PRESSURE_SAMPLE_SIZE];
 extern float waterTemperatureSamples[WATER_TEMP_SAMPLE_SIZE];
 extern float oilTemperatureSamples[OIL_TEMP_SAMPLE_SIZE];
 
-float convertAdcToVoltage(int16_t rawAdc);
-float convertVoltageToOilPressure(float voltage);
-float convertVoltageToTemp(float voltage);
-
 int16_t readAdcWithSettling(uint8_t ch);
 void acquireSensorData();
 
-// 平均計算テンプレート
-template <size_t N>
-inline float calculateAverage(const float (&values)[N])
-{
-    float sum = 0.0f;
-    for (size_t i = 0; i < N; ++i) {
-        sum += values[i];
-    }
-    return sum / static_cast<float>(N);
-}
 
 #endif // SENSOR_H
